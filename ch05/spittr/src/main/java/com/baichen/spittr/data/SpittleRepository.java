007a9336c84381898e12c25112d0ad7ce113077d
Chen Bai <baichenno1@gmail.com>
2019-02-20 09:21:53 +0800

    add Spittle and SpittleRepository

---
+++ ch05/spittr/src/main/java/com/baichen/spittr/data/SpittleRepository.java
@@ -0,0 +1,9 @@
+package com.baichen.spittr.data;
+
+import com.baichen.spittr.Spittle;
+
+import java.util.List;
+
+public interface SpittleRepository {
+    List<Spittle> findSpittles(long max, int count);
+}
