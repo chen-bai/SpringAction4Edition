328243a24cf3d6c25c53feab4ddcabcc27fb7dc2
Chen Bai <baichenno1@gmail.com>
2019-02-23 09:47:32 +0800

    add input validation

--- ch05/spittr/src/main/java/com/baichen/spittr/data/SpitterRepositoryDAO.java
+++ ch05/spittr/src/main/java/com/baichen/spittr/data/SpitterRepositoryDAO.java
@@ -1,15 +1,28 @@
 package com.baichen.spittr.data;
 
 import com.baichen.spittr.Spitter;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class SpitterRepositoryDAO implements SpitterRepository {
+    private List<Spitter> spitters = new ArrayList<>();
+
     @Override
     public Spitter save(Spitter spitter) {
-        return null;
+        spitters.add(spitter);
+        return spitter;
     }
 
     @Override
     public Spitter findByUsername(String username) {
-        return null;
+        for (Spitter spitter : spitters) {
+            if (spitter.getUsername().equals(username)) {
+                return spitter;
+            }
+        }
+        return new Spitter("The User Does Not Exist!", null, null, null);
     }
 }
